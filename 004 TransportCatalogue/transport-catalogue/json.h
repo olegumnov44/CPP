47f54bcf6c140d469536245ef27d674f800bc355
Oleg Umnov <oleg_umnov@mail.ru>
2023-09-13 22:49:12 +0300

    finish

---
+++ 004 TransportCatalogue/transport-catalogue/json.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace json {
+
+class Node;
+
+using Dict = std::map<std::string, Node>;
+using Array = std::vector<Node>;
+
+class ParsingError : public std::runtime_error {
+public:
+    using runtime_error::runtime_error;
+};
+
+class Node final
+    : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> {
+public:
+    using variant::variant;
+    using Value = variant;
+
+    Node(Value value)
+        : variant(std::move(value))
+    {}
+
+    bool IsInt() const;
+    int AsInt() const;
+    bool IsPureDouble() const;
+    bool IsDouble() const;
+    double AsDouble() const;
+    bool IsBool() const;
+    bool AsBool() const;
+    bool IsNull() const;
+    bool IsArray() const;
+    const Array& AsArray() const;
+    bool IsString() const;
+    const std::string& AsString() const;
+    bool IsDict() const;
+    const Dict& AsDict() const;
+
+    bool operator==(const Node& rhs) const {
+        return GetValue() == rhs.GetValue();
+    }
+    const Value& GetValue() const {
+        return *this;
+    }
+    Value& GetValue() {
+        return *this;
+    }
+};
+
+inline bool operator!=(const Node& lhs, const Node& rhs) {
+    return !(lhs == rhs);
+}
+
+class Document {
+public:
+    explicit Document(Node root)
+        : root_(std::move(root))
+    {}
+    const Node& GetRoot() const {
+        return root_;
+    }
+
+private:
+    Node root_;
+};
+
+inline bool operator==(const Document& lhs, const Document& rhs) {
+    return lhs.GetRoot() == rhs.GetRoot();
+}
+
+inline bool operator!=(const Document& lhs, const Document& rhs) {
+    return !(lhs == rhs);
+}
+
+Document Load(std::istream& input);
+
+void Print(const Document& doc, std::ostream& output);
+
+}  // namespace json
